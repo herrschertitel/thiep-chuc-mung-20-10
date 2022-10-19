da1a0630923336a556fab02a3b0034d7a504efc9
hoangtv13 <87896647+hoangtv13@users.noreply.github.com>
2022-10-19 18:28:23 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+const nameGirl = '';
+const giftUrl = '';
+const eventName = `Happy women's day`;
+const titleCard = 'From Saint with love';
+const contentCard = 'Chúc Hà có một ngày lễ 20/10 thật vui vẻ, luôn xinh đẹp và học tập thật tốt. Cố gắng lên nhé, lúc nào Hà cũng sẽ có t bên cạnh :))';
+
+
+const giftImage = 'anh.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
